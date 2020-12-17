8fea8bfe4671d1e990bf43ae5efd4cd1afd494a5
Bob <bkane90@gmail.com>
2020-12-17 18:17:19 -0500

    polish

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,10 @@ import ChartIcon from '../assets/chart-icon.png';
 const Home = () => {
     return (
         <div>
-            <div className='background-image mb-3 d-flex flex-column align-items-center justify-content-center'>
+            <div className='background-image mb-3 d-flex flex-column justify-content-center align-items-center'>
                 <div className='d-flex flex-column align-items-center justify-content-center pt-5 mt-5'>
                     <h1>WELCOME TO CRYPTOPEDIA</h1>
-                    <h2>Trade crypto, without the rnpm sisk</h2>
+                    <h2>Trade crypto, without the risk</h2>
                     <p>Practice trading BTC and other popular cryptocurrencies all in one place. </p>
                     <h6>Think you got what it takes to be the next bitcoin billionaire?</h6>
 
@@ -19,21 +19,24 @@ const Home = () => {
                         <button className='btn btn-primary'><Link className='text-white' to={'/createacc'}>SIGN UP NOW</Link></button>
                     </div>
                 </div>
-                <div className='d-flex m-5 p-5 justify-content-center'>
-                    <div className='d-flex align-items-center container-fluid'>
-                        <img src={CoinStack} className='coinStack' alt='Coin Stack' />
+                <div className='container-fluid'>
+                    <div className="row mt-5">
+                        <div className='col-12 col-md-6 col-lg-2 d-flex align-items-center'>
+                            <img src={CoinStack} className='coinStack' alt='Coin Stack' />
+                        </div>
+                        <div className='col-12 col-md-6 col-lg-4'>
+                            <h5>Compete, Risk Free with $100,000 in Virtual Cash</h5>
+                            <p>Start with $100,000 in virtual cash and put your trading skills to the test. Compete with thousands of other traders and trade your way to the top!</p>
+                        </div>
+                        <div className='col-12 col-md-6 col-lg-2 d-flex align-items-center'>
+                            <img src={ChartIcon} className='chartIcon' alt='Chart Icon' />
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-4">
+                            <h4>A Stepping Stone to the Real Markets</h4>
+                            <p>The ideal platform to get your crypto feet wet! Submit trades in a virtual environment before you start risking your own capital.</p>
+                        </div>
                     </div>
-                    <div className='col-5'>
-                        <h5>Compete, Risk Free with $100,000 in Virtual Cash</h5>
-                        <p>Start with $100,000 in virtual cash and put your trading skills to the test. Compete with thousands of other traders and trade your way to the top!</p>
-                    </div>
-                    <div className='d-flex align-items-center container-fluid'>
-                        <img src={ChartIcon} className='chartIcon' alt='Chart Icon' />
-                    </div>
-                    <div className="col-5">
-                        <h4>A Stepping Stone to the Real Markets</h4>
-                        <p>The ideal platform to get your crypto feet wet! Submit trades in a virtual environment before you start risking your own capital.</p>
-                    </div>
+
                 </div>
             </div>
             <div>
