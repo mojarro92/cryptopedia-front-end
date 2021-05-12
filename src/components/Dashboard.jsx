b9961feaa53adb5c4cba7751584032ef12a84d4a
Alejandro Mojarro <amojarrot@gmail.com>
2021-05-12 15:13:19 -0600

    done4

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,69 @@
+import axios from 'axios'
+import { useEffect, useState, useMemo } from 'react';
+import coinList from '../assets/top100cryptos'
+import Table from './Table'
+
+
 const Dashboard = ({ userInfo }) => {
+
+    const [allRawCryptos, setallRawCryptos] = useState({})
+    const query = "&ids=" + coinList.join(',')
+
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_GECKO_MARKET_INFO + query)
+            .then(res => setallRawCryptos(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    function formatCryptoObj(cryptoApiObject) {
+        let rawObject = Object.keys(cryptoApiObject).map(k => {
+            let coinBalance = userInfo?.coins[k.toLowerCase()]
+                ? userInfo?.coins[k.toLowerCase()]
+                : 0;
+            return {
+                name: k.toUpperCase(),
+                price: Math.floor(allRawCryptos[k].usd * 1000) / 1000,
+                change: Math.floor(allRawCryptos[k].usd_24h_change * 100) / 100,
+                marketCap: Math.floor(allRawCryptos[k].usd_market_cap),
+                coinBalance: coinBalance
+            }
+        })
+        rawObject.sort((a, b) => {
+            if (a.marketCap > b.marketCap)
+                return -1
+            else {
+                return 1
+            }
+        })
+        return rawObject
+    }
+
+    const columns = useMemo(() => [
+        {
+            Header: 'Cryptocurency Information',
+            columns: [
+                {
+                    Header: 'Identifier',
+                    accessor: 'name'
+                },
+                {
+                    Header: 'Price',
+                    accessor: 'price'
+                },
+                {
+                    Header: '24h %',
+                    accessor: 'change'
+                },
+                {
+                    Header: 'Market Cap',
+                    accessor: 'marketCap'
+                }
+            ]
+        }
+
+    ], [])
+
+    const data = useMemo(() => formatCryptoObj(allRawCryptos), [allRawCryptos])
 
     return (
         <>
@@ -13,14 +78,16 @@ const Dashboard = ({ userInfo }) => {
                     </div>
                     <div className='col'>
                         <h3 className='text-center'>Market</h3>
+                        <Table
+                            data={data}
+                            columns={columns}
+                        />
                     </div>
                     <div className='col'>
                         <h3>Trading History</h3>
                         <div>
-
                         </div>
                     </div>
-
                 </div>
             </div>
         </>
---
+++ src/components/Table.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import RTable from 'react-bootstrap/Table'
+import { useTable, useSortBy, usePagination } from 'react-table';
+
+function Table({ data, columns, updateMyData }) {
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable(
+        {
+            columns,
+            data,
+        },
+        useSortBy,
+    )
+
+    return (
+        <RTable striped bordered {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        { headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                { column.render('Header')}
+                            </th>
+                        ))}
+                    </tr>))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            { row.cells.map(cell => {
+                                return (
+                                    <td {...cell.getCellProps()}>
+                                        { cell.render("Cell")}
+
+                                    </td>
+                                )
+                            })}
+                        </tr>)
+                })}
+            </tbody>
+        </RTable>
+    )
+}
+
+export default Table
